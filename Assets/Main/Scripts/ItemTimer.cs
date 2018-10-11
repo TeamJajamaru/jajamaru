069dc352cf5e9467a1fabef9f025410b06965feb
ou <kamijoukyo@gmail.com>
2018-10-11 16:03:01 +0900

    アイテム系のスクリプト

---
+++ Assets/Main/Scripts/ItemTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTimer : Item {
+    Timer _timer = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDestroy(){
+        
+    }
+}
---
+++ Assets/Main/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    float _time_limit = 60;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _time_limit -= Time.deltaTime;
+        if (_time_limit <= 0) {
+            SceneChanger.sceneChange("Result");
+        }
+	}
+
+    public void addTime(float time) {
+        _time_limit += time;
+    }
+}
